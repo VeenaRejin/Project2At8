8d995c2a2b4c19e874bf2079c7f56214c72a1ca2
veena rejin <veena rejin@LAPTOP-P0J58E7F>
2024-01-14 22:43:12 +0530

    first commit

---
+++ Project2At8/src/main/java/constant/Constant.java
@@ -0,0 +1,8 @@
+package constant;
+
+public class Constant {
+public static final String screenshotpath=System.getProperty("user.dir")+"\\Screenshot\\";
+public static final String excelpath=System.getProperty("user.dir")+"\\src\\test\\resources\\Excel2.xlsx\\";
+public static final String propertiespath=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties\\";
+	
+}
